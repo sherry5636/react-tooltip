6dcd385ebba66e871976153193969b911350759e
zhangyumeng <zhangyumeng@megvii.com>
2019-07-10 20:55:43 +0800

    demo

--- example/src/index.js
+++ example/src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { render} from 'react-dom';
 import Tooltip from '../../src';
 
-render(<Tooltip position='top' content={'这是提示框的内容'}>
-<button>提示框</button>
-</Tooltip>, document.getElementById("tooltip-demo"));
+render(<>
+    <Tooltip content={'left提示框'} position="left" style={{margin: '20px'}}>
+        <button >提示框</button>
+    </Tooltip>
+    <Tooltip content={'top提示框'} style={{margin: '20px'}}>
+        <button >提示框</button>
+    </Tooltip>
+    <Tooltip content={'bottom提示框'} position="bottom" style={{margin: '20px'}}>
+        <button >提示框</button>
+    </Tooltip>
+    <Tooltip content={'right提示框'} position="right" style={{margin: '20px'}}>
+        <button >提示框</button>
+    </Tooltip>
+</>, document.getElementById("tooltip-demo"));
